35ca459086914ad3d7710f8d2b5fd7c6896c7017
mr-juul <62436596+mr-juul@users.noreply.github.com>
2020-03-21 12:10:15 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,24 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
-
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-// `client.on('...')` events and such below this point
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
+// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
 
