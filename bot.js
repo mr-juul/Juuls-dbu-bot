76700903c5b7ef275324eac9ede943decc426b6a
mr-juul <62436596+mr-juul@users.noreply.github.com>
2020-03-21 14:46:23 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,6 +6,3 @@ const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
 // Set the client user's activity
-client.user.setActivity('DBU', { type: 'WATCHING' })
-  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-  .catch(console.error);
