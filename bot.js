4b87c41a4781637d372ca19e267547ef83c72cb8
mr-juul <62436596+mr-juul@users.noreply.github.com>
2020-03-21 11:54:25 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -37,9 +37,3 @@ client.on('message', msg => {
     msg.reply('Ja?');
   }
 });
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-bot.on("ready", () => {
-  console.log("Ready");
-  bot.user.setActivity("DBU");
-})
